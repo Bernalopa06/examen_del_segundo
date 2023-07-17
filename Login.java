c536f39cfe911aecb494d9cd97827a0d55aba494
Guillermo <lopezbernardo101@gmail.com>
2023-07-17 16:49:59 -0600

    Primera subida

---
+++ Login.java
@@ -0,0 +1,3 @@
+public void login(Request request){
+    //Aqui va el codigo que recoge las credenciales de usuario
+}
---
+++ Registro.java
@@ -0,0 +1,3 @@
+public void registro(Request request){
+    //Aqui va el codigo que recoge las credenciales de usuario
+}
